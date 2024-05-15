50878a360bcf5fc951273a2b85782ac39d3236d7
Serotina <wlsdn2749@gmail.com>
2024-05-16 00:20:31 +0900

    Modify: astro.config.mjs, base from ... to ''

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://wlsdn2749.github.io',
-	base: 'astro-blog',
+	base: '',
 	integrations: [mdx(), sitemap()],
 });
