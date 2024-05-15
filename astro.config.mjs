56784bc4f80a43db43a89e938d03612a533f4c5c
Serotina <wlsdn2749@gmail.com>
2024-05-15 22:49:18 +0900

    Update: Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://serotina.github.io',
+	base: 'astro-blog',
 	integrations: [mdx(), sitemap()],
 });
