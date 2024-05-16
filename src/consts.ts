9ea7511a0a45db2e40ede2fe1cd824fbc8ad0ab5
Serotina <wlsdn2749@gmail.com>
2024-05-16 15:35:14 +0900

    Update: Social-links

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Serotina Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_GITHUB = 'https://github.com/wlsdn2749';
+export const SITE_LINKEDIN = 'https://www.linkedin.com/in/serotina/';
+export const SITE_NOTION_STUDY = 'https://serotina.notion.site/Personal-Study-Pages-e40cbcabb73348e299311aa526252b78?pvs=4'
